2315775464bcca2f9c8d9d635916d559299b632a
Emanuele Bianchi <emanuelebianchi00@gmail.com>
2025-03-27 18:21:06 +0100

    Main

---
+++ src/main/java/com/epicode/ticketvision/TicketvisionApplication.java
@@ -0,0 +1,13 @@
+package com.epicode.ticketvision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketvisionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketvisionApplication.class, args);
+	}
+
+}
